aeeb4fb57537b8f46484a72fcfa3bc7437ee1048
nareshsingh701 <Kallanpur@701>
2022-05-15 15:17:15 +0530

    i learn StatusBar1

---
+++ src/sugreev/StatusBar1.js
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet, StatusBar, SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+
+
+class StatusBar1 extends Component {
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <StatusBar
+                    backgroundColor="blue"
+                    barStyle='dark-content'
+                />
+            </SafeAreaView>
+        )
+    }
+}
+export default StatusBar1;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
